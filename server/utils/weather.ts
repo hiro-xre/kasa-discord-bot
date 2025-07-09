e3945b09bd0810c6b6dca8433b649ba721d056fe
hiroto-hara <xre0605@gmail.com>
2025-07-09 21:10:04 +0900

    feat: 最高降水確率を計算する

--- server/utils/weather.ts
+++ server/utils/weather.ts
@@ -22,3 +22,36 @@ const fetchWeatherForecast = async (): Promise<WeatherAPIResponse> => {
 		throw new Error("天気情報の取得に失敗しました");
 	}
 };
+
+type TimeSlotRainChance = {
+	morning: string; // 0~6時
+	noon: string; // 6~12時
+	evening: string; // 12~18時
+	night: string; // 18~24時
+};
+
+export const getTodayRainChance = async (): Promise<TimeSlotRainChance> => {
+	const response = await fetchWeatherForecast();
+	const todayForecast = response.forecasts[0];
+
+	return {
+		morning: todayForecast.chanceOfRain.T00_06,
+		noon: todayForecast.chanceOfRain.T06_12,
+		evening: todayForecast.chanceOfRain.T12_18,
+		night: todayForecast.chanceOfRain.T18_24,
+	};
+};
+
+const convertRainChancesToNumber = (
+	rainChances: TimeSlotRainChance,
+): number[] => {
+	return Object.values(rainChances).map((chance) => Number.parseInt(chance));
+};
+
+const calculateMaxRainChance = (rainChances: TimeSlotRainChance): number => {
+	const chances = convertRainChancesToNumber(rainChances).map((chance) =>
+		Number.isNaN(chance) ? 0 : chance,
+	);
+
+	return Math.max(...chances);
+};
